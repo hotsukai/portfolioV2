cb0b918631e79856a0dd1ef1eee0dbf8497e56f0
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2021-12-02 17:08:10 +0900

    Add pretttier

---
+++ .prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  printWidth: 80,
+  arrowParens: "always",
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  quoteProps: "consistent",
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  insertPragma: false,
+  proseWrap: "preserve", //markdownの折返しの設定
+  tabWidth: 2,
+  trailingComma: "es5",
+  vueIndentScriptAndStyle: false,
+  parser: "none",
+};
