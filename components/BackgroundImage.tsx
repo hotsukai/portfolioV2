bcdac79f4f773ae7b1a034dd9af8323fda4c4c72
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2021-12-02 18:51:34 +0900

    Update: 画像を切り替え

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,28 +1,38 @@
 import { useState, useEffect } from "react";
 
 const images = ["images/forest.jpg", "images/bridge.jpg"];
+
 const BackGroundImage = () => {
   const [count, setCount] = useState(0);
+  const activeImageIndex = count % images.length;
   useEffect(() => {
     const interval = setInterval(() => {
       setCount((c) => c + 1);
-    }, 1000);
+    }, 5000);
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-        backgroundImage: `linear-gradient(to top, rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url("${
-          images[count % images.length]
-        }") `,
-        backgroundSize: "cover",
-        zIndex: -1,
-        position: "fixed",
-      }}
-    ></div>
+    <>
+      {images.map((image, i) => (
+        <div
+          style={{
+            height: "100%",
+            width: "95%",
+            backgroundImage: `linear-gradient(to top, rgba(0,0,0,0.2), rgba(0,0,0,0.2)), url("${
+              images[i]
+            }") `,
+            backgroundSize: "cover",
+            opacity: i === activeImageIndex ? 1 : 0,
+            zIndex: -1,
+            position: "fixed",
+            transition: "opacity 1s 0s linear",
+            overflow:"hidden"
+          }}
+          key={image}
+        />
+      ))}
+    </>
   );
 };
 
