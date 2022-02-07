e9ab4fffe1409639caafd6f75e1ae5d41d0e0dc1
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2022-02-07 19:00:55 +0900

    add metainfo

--- pages/_documents.tsx
+++ pages/_documents.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="ja">
         <Head>
           <link
             href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@300&display=swap"
