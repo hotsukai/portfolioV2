2dc0ff7a6ad232d5a85b97b32257f101ea99f9c1
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2022-02-12 22:52:17 +0900

    update richi text

--- components/RichText.tsx
+++ components/RichText.tsx
@@ -5,9 +5,12 @@ type Props = { richText: RichText };
 const RichTextArea: FC<Props> = ({ richText }) => {
   if (richText.type === "p") return <p>{richText.body}</p>;
   if (richText.type === "p-mb") return <p className="mb-4">{richText.body}</p>;
-  if (richText.type === "h1") return <h1>{richText.body}</h1>;
-  if (richText.type === "h2") return <h2>{richText.body}</h2>;
-  if (richText.type === "h3") return <h3>{richText.body}</h3>;
+  if (richText.type === "h1")
+    return <h1 className="text-xxl">{richText.body}</h1>;
+  if (richText.type === "h2")
+    return <h2 className="text-xl">{richText.body}</h2>;
+  if (richText.type === "h3")
+    return <h3 className="text-lg">{richText.body}</h3>;
   return <></>;
 };
 export default RichTextArea;
