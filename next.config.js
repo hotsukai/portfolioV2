6e7431aa20237d4d5bf65a167e942c3d64f55b55
Kaihotsu Kotaro <kaihotsu.kotaro@gmail.com>
2022-02-20 23:48:58 +0900

    update image cache test

--- next.config.js
+++ next.config.js
@@ -8,5 +8,7 @@ module.exports = {
       "www.notion.so",
       "s3.us-west-2.amazonaws.com",
     ],
+
+    minimumCacheTTL: 60 * 60 * 24 * 365, // Next.js側で画像のキャッシュを1年間やってもらう。Notionの画像URLは1時間で無効になるためにNext側で画像の実態を保つ必要がある。
   },
 };
